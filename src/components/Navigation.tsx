9cd3ae4153355e88288c3ac6ea283d59b11eb039
SidhardhCR <sidhardhcr007@gmail.com>
2025-10-13 22:06:49 +0530

    updated 1

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -55,7 +55,7 @@ export function Navigation() {
             <img
               src={theme === 'dark' ? '/logo_white.png' : '/logo_black.png'}
               alt="Talent Advertising Logo"
-              className="h-12 w-full"
+              className="h-10 w-auto max-w-[140px] sm:h-12 sm:max-w-[180px]"
               draggable={false}
             />
           </motion.div>
